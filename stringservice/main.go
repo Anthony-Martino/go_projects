7b0c53205ac754ea83dbb648add0f7c25f3d74bd
Anthony Martino <Amartino@lb.com>
2021-01-16 12:31:42 -0500

    Add service and initial setup

---
+++ stringservice/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"errors"
+	"strings"
+)
+
+type StringService interface {
+	Uppercase(string) (string, error)
+	Count(string) int
+}
+
+type stringService struct{}
+
+func (stringService) Uppercase(s string) (string, error) {
+	if s == "" {
+		return "", ErrEmpty
+	}
+
+	return strings.ToUpper(s), nil
+}
+
+func (stringService) Count(s string) int {
+	return len(s)
+}
+
+func main() {
+
+}
+
+// ErrEmpty is returned when input string is empty
+var ErrEmpty = errors.New("Empty string")
+
+
